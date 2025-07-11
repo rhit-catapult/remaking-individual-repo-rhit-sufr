2b63b51b7944c4345390b1e53c2f755793bc608a
rhit-sufr <SUNB2@rose-hulman.edu>
2025-07-11 22:41:39 +0800

    BouncingBall

--- 05-BouncingBall/BouncingBall.py
+++ 05-BouncingBall/BouncingBall.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import random
+import math
 
 WIDTH = 2560
 HEIGHT = 1440
@@ -13,13 +14,13 @@ HEIGHT = 1440
 # TODO: Methods: __init__, draw, move
 
 class Ball:
-    def __init__(self, screen, x, y):
+    def __init__(self, screen, x, y, radius, speed_x, speed_y):
         self.screen = screen
         self.x = x
         self.y = y
-        self.radius = 10
-        self.speed_x = random.randint(-10, 10)
-        self.speed_y = random.randint(-10, 10)
+        self.radius = radius
+        self.speed_x = speed_x
+        self.speed_y = speed_y
 
     def draw(self):
         pygame.draw.circle(self.screen, (255, 255, 255), (self.x, self.y), self.radius)
@@ -27,6 +28,60 @@ class Ball:
     def move(self):
         self.x += self.speed_x
         self.y += self.speed_y
+
+
+class Balls:
+    def __init__(self, screen):
+        self.screen = screen
+        self.balls = []
+
+    def generate(self, x, y, radius, speed_x, speed_y):
+        new_ball = Ball(self.screen, x, y, radius, speed_x, speed_y)
+        self.balls.append(new_ball)
+
+    def delete(self, index):
+        self.balls.pop(index)
+
+
+class Player:
+    def __init__(self, screen, x, y, time):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.time = time
+
+    def draw(self):
+        pygame.draw.circle(self.screen, (255, 255, 255), (self.x, self.y), 20)
+
+    def move(self, keys):
+        vec_x = 0
+        vec_y = 0
+
+        if keys[pygame.K_w]:
+            vec_y -= 1
+        if keys[pygame.K_s]:
+            vec_y += 1
+        if keys[pygame.K_a]:
+            vec_x -= 1
+        if keys[pygame.K_d]:
+            vec_x += 1
+
+        if vec_x != 0 and vec_y != 0:
+            vec_x = math.sqrt(2)
+            vec_y = math.sqrt(2)
+
+        self.x += vec_x * 15
+        self.y += vec_y * 15
+
+        if self.x < 0:
+            self.x = 0
+        if self.x > WIDTH:
+            self.x = WIDTH
+        if self.y < 0:
+            self.y = 0
+        if self.y > HEIGHT:
+            self.y = HEIGHT
+
 
 def main():
     pygame.init()
@@ -36,7 +91,10 @@ def main():
     clock = pygame.time.Clock()
 
     # TODO: Create an instance of the Ball class called ball1
-    ball0 = Ball(screen, WIDTH / 2, HEIGHT / 2)
+    ball_list = Balls(screen)
+    ball_list.generate(WIDTH / 2, HEIGHT / 2, 10, random.randint(-10, 10), random.randint(-10, 10))
+
+    player = Player(screen, WIDTH / 2, HEIGHT / 4, 0)
 
     while True:
         for event in pygame.event.get():
@@ -44,31 +102,47 @@ def main():
                 sys.exit()
 
         clock.tick(60)
+        player.time += 1
         screen.fill(pygame.Color('gray'))
 
-        # TODO: Move the ball
-        if ball0.x < 0:
-            ball0.x = 0
-            ball0.speed_x = -ball0.speed_x
-        if ball0.x > WIDTH:
-            ball0.x = WIDTH
-            ball0.speed_x = -ball0.speed_x
-        if ball0.y < 0:
-            ball0.y = 0
-            ball0.speed_y = -ball0.speed_y
-        if ball0.y > HEIGHT:
-            ball0.y = HEIGHT
-            ball0.speed_y = -ball0.speed_y
-        ball0.move()
+        for one_ball in ball_list.balls:
+            # TODO: Move the ball
+            if one_ball.x < 0:
+                one_ball.x = 0
+                one_ball.speed_x = -one_ball.speed_x
+                ball_list.generate(one_ball.x, one_ball.y, one_ball.radius, one_ball.speed_x + random.randint(-2, 2),
+                                   one_ball.speed_y + random.randint(-2, 2))
+            if one_ball.x > WIDTH:
+                one_ball.x = WIDTH
+                one_ball.speed_x = -one_ball.speed_x
+                ball_list.generate(one_ball.x, one_ball.y, one_ball.radius, one_ball.speed_x + random.randint(-2, 2),
+                                   one_ball.speed_y + random.randint(-2, 2))
+            if one_ball.y < 0:
+                one_ball.y = 0
+                one_ball.speed_y = -one_ball.speed_y
+                ball_list.generate(one_ball.x, one_ball.y, one_ball.radius, one_ball.speed_x + random.randint(-2, 2),
+                                   one_ball.speed_y + random.randint(-2, 2))
+            if one_ball.y > HEIGHT:
+                one_ball.y = HEIGHT
+                one_ball.speed_y = -one_ball.speed_y
+                ball_list.generate(one_ball.x, one_ball.y, one_ball.radius, one_ball.speed_x + random.randint(-2, 2),
+                                   one_ball.speed_y + random.randint(-2, 2))
+            one_ball.move()
 
-        # TODO: Draw the ball
-        ball0.draw()
+            player.move(pygame.key.get_pressed())
+
+            # TODO: Draw the ball
+            one_ball.draw()
+            player.draw()
+
+        # print(len(ball_list.balls))
+        if len(ball_list.balls) > 50:
+            ball_list.delete(random.randint(0, 50))
 
         pygame.display.update()
 
 
 main()
-
 
 # Optional challenges (if you finish and want do play a bit more):
 #   After you get 1 ball working make a few balls (ball2, ball3, etc) that start in different places.
--- main.py
+++ main.py
@@ -1,137 +1,123 @@
-    # # from pickle import REDUCE
-    #
-    # import pygame
-    # import sys
-    # import math
-    # import random
-    #
-    # # Initialize Pygame
-    # pygame.init()
-    #
-    # # Screen dimensions
-    # WIDTH, HEIGHT = 3200, 2000
-    # pygame.display.init()
-    # screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.DOUBLEBUF|pygame.FULLSCREEN, vsync=True)
-    # clock = pygame.time.Clock()
-    # pygame.display.set_caption("Pygame Setup Test")
-    #
-    # # Colors
-    # WHITE = pygame.Color('#eceff4')
-    # RED = pygame.Color('#bf616a')
-    # ORANGE = pygame.Color('#d08770')
-    # YELLOW = pygame.Color('#ebcb8b')
-    # GREEN = pygame.Color('#a3be8c')
-    # PURPLE = pygame.Color('#b48ead')
-    #
-    # # Game loop
-    # running = True
-    # dt = 0
-    # player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-    #
-    # #Loading Assets
-    # image1 = pygame.image.load("02-DogBark/2dogs.JPG")
-    # font1 = pygame.font.SysFont("jetbrains mono", 200)
-    # caption1 = font1.render("DogBark", True, WHITE)
-    # bark_sound = pygame.mixer.Sound("02-DogBark/bark.wav")
-    # pygame.mixer.music.load("03-ClickInTheCircle/drums.wav")
-    #
-    # def distance(p1, p2):
-    #     return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
-    #
-    # caption1_flag = 0
-    #
-    # while running:
-    #     for event in pygame.event.get():
-    #         if event.type == pygame.QUIT:
-    #             running = False
-    #         if event.type == pygame.VIDEORESIZE:
-    #             # Recreate the display surface with the new size
-    #             screen = pygame.display.set_mode((event.w, event.h), pygame.DOUBLEBUF|pygame.FULLSCREEN, vsync=True)
-    #             WIDTH, HEIGHT = event.w, event.h
-    #         if event.type == pygame.MOUSEBUTTONDOWN:
-    #             bark_sound.play()
-    #             print(distance(player_pos, pygame.mouse.get_pos()))
-    #             if distance(player_pos, pygame.mouse.get_pos()) > 40:
-    #                 print("Outside")
-    #                 caption1_flag = 150
-    #                 pygame.mixer.music.stop()
-    #             else:
-    #                 pygame.mixer.music.play(-1)
-    #
-    #     # Drawing
-    #     screen.fill(WHITE)
-    #
-    #     screen.blit(image1, (500, 250))
-    #     if caption1_flag > 0:
-    #         screen.blit(caption1, (500, 250))
-    #
-    #     pygame.draw.rect(screen, RED, (710, 490, 100, 100)) # Draw a blue square
-    #     pygame.draw.rect(screen, ORANGE, (810, 490, 100, 100)) # Draw a blue square
-    #     pygame.draw.rect(screen, YELLOW, (910, 490, 100, 100)) # Draw a blue square
-    #     pygame.draw.rect(screen, GREEN, (1010, 490, 100, 100)) # Draw a blue square
-    #     pygame.draw.rect(screen, PURPLE, (1110, 490, 100, 100)) # Draw a blue square
-    #     pygame.draw.circle(screen, RED, (760, 450), 50) # Draw a blue square
-    #     pygame.draw.circle(screen, ORANGE, (860, 450), 50) # Draw a blue square
-    #     pygame.draw.circle(screen, YELLOW, (960, 450), 50) # Draw a blue square
-    #     pygame.draw.circle(screen, GREEN, (1060, 450), 50) # Draw a blue square
-    #     pygame.draw.circle(screen, PURPLE, (1160, 450), 50) # Draw a blue square
-    #
-    #     pygame.draw.circle(screen, (255, 255, 0), (320, 240), 210)  # yellow circle
-    #     pygame.draw.circle(screen, (0, 0, 0), (320, 240), 210, 4)  # black outline
-    #     pygame.draw.circle(screen, (225, 225, 225), (240, 160), 25)  # white eye
-    #     pygame.draw.circle(screen, (0, 0, 0), (240, 160), 25, 3)  # black outline
-    #     pygame.draw.circle(screen, (0, 0, 0), (242, 162), 7)  # black pupil
-    #     pygame.draw.circle(screen, (225, 225, 225), (400, 160), 25)  # white eye
-    #     pygame.draw.circle(screen, (0, 0, 0), (400, 160), 25, 3)  # black outline
-    #     pygame.draw.circle(screen, (0, 0, 0), (398, 162), 7)  # black pupil
-    #
-    #     pygame.draw.circle(screen, "red", player_pos, 40)
-    #
-    #     keys = pygame.key.get_pressed()
-    #     if keys[pygame.K_w]:
-    #         player_pos.y -= 300 * dt
-    #     if keys[pygame.K_s]:
-    #         player_pos.y += 300 * dt
-    #     if keys[pygame.K_a]:
-    #         player_pos.x -= 300 * dt
-    #     if keys[pygame.K_d]:
-    #         player_pos.x += 300 * dt
-    #
-    #     player_pos.x += random.randint(0, 6) - 3
-    #     player_pos.y += random.randint(0, 6) - 3
-    #
-    #     if player_pos.x < 0:
-    #         player_pos.x = 0
-    #     if player_pos.x > WIDTH:
-    #         player_pos.x = WIDTH
-    #     if player_pos.y < 0:
-    #         player_pos.y = 0
-    #     if player_pos.y > HEIGHT:
-    #         player_pos.y = HEIGHT
-    #
-    #     caption1_flag -= 1
-    #
-    #     # Update the display
-    #     pygame.display.flip()
-    #
-    #     dt = clock.tick(300) / 1000
-    #
-    # # Quit
-    # pygame.quit()
-    # sys.exit()
+# from pickle import REDUCE
 
-class Cloud:
-    def __init__(self, x, y, vx, vy, image):
-        self.x = x
-        self.y = y
-        self.vx = vx
-        self.vy = vy
-        self.image = image
+import pygame
+import sys
+import math
+import random
 
-    def move(self, dt):
-        self.x += self.vx * dt
-        self.y += self.vy * dt
+# Initialize Pygame
+pygame.init()
 
-    # def draw(self, screen):
-    #
-    # def make_rain(self, rain):
+# Screen dimensions
+WIDTH, HEIGHT = 3200, 2000
+pygame.display.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.DOUBLEBUF | pygame.FULLSCREEN, vsync=True)
+clock = pygame.time.Clock()
+pygame.display.set_caption("Pygame Setup Test")
+
+# Colors
+WHITE = pygame.Color('#eceff4')
+RED = pygame.Color('#bf616a')
+ORANGE = pygame.Color('#d08770')
+YELLOW = pygame.Color('#ebcb8b')
+GREEN = pygame.Color('#a3be8c')
+PURPLE = pygame.Color('#b48ead')
+
+# Game loop
+running = True
+dt = 0
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+# Loading Assets
+image1 = pygame.image.load("02-DogBark/2dogs.JPG")
+font1 = pygame.font.SysFont("jetbrains mono", 200)
+caption1 = font1.render("DogBark", True, WHITE)
+bark_sound = pygame.mixer.Sound("02-DogBark/bark.wav")
+pygame.mixer.music.load("03-ClickInTheCircle/drums.wav")
+
+
+def distance(p1, p2):
+    return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+
+
+caption1_flag = 0
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.VIDEORESIZE:
+            # Recreate the display surface with the new size
+            screen = pygame.display.set_mode((event.w, event.h), pygame.DOUBLEBUF | pygame.FULLSCREEN, vsync=True)
+            WIDTH, HEIGHT = event.w, event.h
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            bark_sound.play()
+            print(distance(player_pos, pygame.mouse.get_pos()))
+            if distance(player_pos, pygame.mouse.get_pos()) > 40:
+                print("Outside")
+                caption1_flag = 150
+                pygame.mixer.music.stop()
+            else:
+                pygame.mixer.music.play(-1)
+
+    # Drawing
+    screen.fill(WHITE)
+
+    screen.blit(image1, (500, 250))
+    if caption1_flag > 0:
+        screen.blit(caption1, (500, 250))
+
+    pygame.draw.rect(screen, RED, (710, 490, 100, 100))  # Draw a blue square
+    pygame.draw.rect(screen, ORANGE, (810, 490, 100, 100))  # Draw a blue square
+    pygame.draw.rect(screen, YELLOW, (910, 490, 100, 100))  # Draw a blue square
+    pygame.draw.rect(screen, GREEN, (1010, 490, 100, 100))  # Draw a blue square
+    pygame.draw.rect(screen, PURPLE, (1110, 490, 100, 100))  # Draw a blue square
+    pygame.draw.circle(screen, RED, (760, 450), 50)  # Draw a blue square
+    pygame.draw.circle(screen, ORANGE, (860, 450), 50)  # Draw a blue square
+    pygame.draw.circle(screen, YELLOW, (960, 450), 50)  # Draw a blue square
+    pygame.draw.circle(screen, GREEN, (1060, 450), 50)  # Draw a blue square
+    pygame.draw.circle(screen, PURPLE, (1160, 450), 50)  # Draw a blue square
+
+    pygame.draw.circle(screen, (255, 255, 0), (320, 240), 210)  # yellow circle
+    pygame.draw.circle(screen, (0, 0, 0), (320, 240), 210, 4)  # black outline
+    pygame.draw.circle(screen, (225, 225, 225), (240, 160), 25)  # white eye
+    pygame.draw.circle(screen, (0, 0, 0), (240, 160), 25, 3)  # black outline
+    pygame.draw.circle(screen, (0, 0, 0), (242, 162), 7)  # black pupil
+    pygame.draw.circle(screen, (225, 225, 225), (400, 160), 25)  # white eye
+    pygame.draw.circle(screen, (0, 0, 0), (400, 160), 25, 3)  # black outline
+    pygame.draw.circle(screen, (0, 0, 0), (398, 162), 7)  # black pupil
+
+    pygame.draw.circle(screen, "red", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
+    player_pos.x += random.randint(0, 6) - 3
+    player_pos.y += random.randint(0, 6) - 3
+
+    if player_pos.x < 0:
+        player_pos.x = 0
+    if player_pos.x > WIDTH:
+        player_pos.x = WIDTH
+    if player_pos.y < 0:
+        player_pos.y = 0
+    if player_pos.y > HEIGHT:
+        player_pos.y = HEIGHT
+
+    caption1_flag -= 1
+
+    # Update the display
+    pygame.display.flip()
+
+    dt = clock.tick(300) / 1000
+
+# Quit
+pygame.quit()
+sys.exit()
